791ef9f8cd39413ed923858f60c16f7221337dba
tao <1445709137@qq.com>
2024-07-31 16:02:04 +0800

    feat: ✨ babel编译和iframe预览

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Allotment: 实现编辑预览布局拖动改变大小
 
 Context 封装全局数据
 点击 tab 的时候切换 selectedFileName，从而切换编辑器的内容
+
+@babel/standalone 做的 tsx 代码的编译，编译过程中需要对 .tsx、.css、.json 等模块的 import 做处理，变成 blob url 的方式
+tsx 模块直接用 babel 编译，css 模块包一层代码加到 head 的 style 标签里，json 包一层代码直接 export 即可
+react、react-dom/client 这种，用浏览器的 import maps 来引入
+iframe.html 的内容替换 import maps 和 src 部分后，同样用 blob url 设置为 iframe 的 src 就可以了
--- src/ReactPlayground/components/Preview/index.tsx
+++ src/ReactPlayground/components/Preview/index.tsx
@@ -1,4 +1,54 @@
-export default function Preview() {
-  return <div>Preview</div>
-}
+import { useContext, useEffect, useState } from 'react'
+import { PlaygroundContext } from '../../PlaygroundContext'
+import { IMPORT_MAP_FILE_NAME } from '../../files'
+import { compile } from './compiler'
+import iframeRaw from './iframe.html?raw'
 
+export default function Preview() {
+  const { files } = useContext(PlaygroundContext)
+  const [compiledCode, setCompiledCode] = useState('')
+
+  useEffect(() => {
+    const res = compile(files)
+    setCompiledCode(res)
+  }, [files])
+
+  const getIframeUrl = () => {
+    console.log(files)
+    const res = iframeRaw
+      .replace(
+        '<script type="importmap"></script>',
+        `<script type="importmap">${files[IMPORT_MAP_FILE_NAME].value}</script>`
+      )
+      .replace(
+        '<script type="module" id="appSrc"></script>',
+        `<script type="module" id="appSrc">${compiledCode}</script>`
+      )
+    return URL.createObjectURL(new Blob([res], { type: 'text/html' }))
+  }
+
+  useEffect(() => {
+    setIframeUrl(getIframeUrl())
+  }, [files[IMPORT_MAP_FILE_NAME].value, compiledCode])
+
+  const [iframeUrl, setIframeUrl] = useState(getIframeUrl())
+
+  return (
+    <div style={{ height: '100%' }}>
+      <iframe
+        src={iframeUrl}
+        style={{
+          width: '100%',
+          height: '100%',
+          padding: 0,
+          border: 'none'
+        }}
+      />
+      {/* <Editor file={{
+            name: 'dist.js',
+            value: compiledCode,
+            language: 'javascript'
+        }}/> */}
+    </div>
+  )
+}
